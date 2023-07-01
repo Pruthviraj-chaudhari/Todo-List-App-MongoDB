83140ceb0605675eafd0880bd36e22d91934a47e
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2023-07-01 15:11:18 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const date = require(__dirname + "/date.js");
+require('dotenv').config();
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {
+const URL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.mtwlrzs.mongodb.net/todolistDB`;
+mongoose.connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+});
+
 
 const itemSchema = new mongoose.Schema({
     name: String
@@ -20,14 +24,12 @@ const itemSchema = new mongoose.Schema({
 const Item = mongoose.model("Item", itemSchema);
 
 const defaultItems = [
-    { name: "Welcome to your todolist!" },
-    { name: "Hit the + button to add a new item." },
-    { name: "<-- Hit this to delete an item." }
+    { name: "Welcome to your todolist!" }
 ]
 
 const listSchema = {
     name: String,
-    list: [itemSchema]
+    items: [itemSchema]
 }
 
 const List = mongoose.model('List', listSchema);
@@ -52,18 +54,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:customListName', (req, res) => {
-    const customListName = req.params.customListName;
+    const customListName = _.capitalize(req.params.customListName);
 
     List.findOne({ name: customListName })
         .then((foundList) => {
             if (foundList) {
                 // Show an existing list
-                res.render("list", { listTitle: foundList.name, listArray: foundList.list });
+                res.render("list", { listTitle: foundList.name, listArray: foundList.items });
             } else {
                 // Create a new list
                 const listItem = new List({
                     name: customListName,
-                    list: defaultItems
+                    items: defaultItems
                 })
                 listItem.save()
                     .then(() => { console.log("New List Created.") })
@@ -97,19 +99,15 @@ app.post('/', (req, res) => {
                     // Handle the error here (e.g., redirect to an error page)
                 });
         } else {
-            List.findOne({ name: listName })
-                .then((found) => {
-                    if (found) {
-                        found.list.push(item);
-                        found.save()
-                            .then(() => {
-                                console.log("New Item Added to Custom List");
-                                res.redirect('/' + listName);
-                            })
-                            .catch(err => {
-                                console.log("Error Saving Custom List:", err);
-                                // Handle the error here (e.g., redirect to an error page)
-                            });
+            List.findOneAndUpdate(
+                { name: listName },
+                { $push: { items: item } },
+                { new: true }
+            )
+                .then(updatedList => {
+                    if (updatedList) {
+                        console.log("New Item Added to Custom List");
+                        res.redirect('/' + listName);
                     } else {
                         console.log("Custom List Not Found");
                         // Handle the case when the custom list is not found
@@ -117,23 +115,41 @@ app.post('/', (req, res) => {
                     }
                 })
                 .catch(err => {
-                    console.log("Error finding Custom ListName:", err);
+                    console.log("Error finding/updating Custom List:", err);
                     // Handle the error here (e.g., redirect to an error page)
                 });
         }
     }
 });
 
-
 app.post('/delete', (req, res) => {
-    Item.deleteOne({ _id: req.body.checkboxID })
-        .then(() => {
-            console.log("Item deleted from database");
-            res.redirect('/');
-        }).catch(err => { console.log(err) });
+
+    const checkboxID = req.body.checkboxID;
+    const listName = req.body.listName;
+
+    if (listName === formatedDate) {
+        Item.deleteOne({ _id: checkboxID })
+            .then(() => {
+                console.log("Item deleted from database");
+                res.redirect('/');
+            }).catch(err => { console.log(err) });
+    } else {
+        List.findOneAndUpdate(
+            { name: listName },
+            { $pull: { items: { _id: checkboxID } } }
+        )
+            .then(() => {
+                console.log("Item removed from the list");
+                res.redirect('/' + listName);
+            })
+            .catch(err => {
+                console.log("Error removing item from the list:", err);
+                // Handle the error here (e.g., redirect to an error page)
+            });
+    }
 })
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server Listening on port 3000");
 });
